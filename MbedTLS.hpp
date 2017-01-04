f601a67d39458d1efc7c0be4ff341694f4fb1611
Malachi Burke <malachi.burke@gmail.com>
2017-01-04 12:30:07 -0800

    Moving NetContext into its own file

--- MbedTLS.hpp
+++ MbedTLS.hpp
@@ -94,39 +94,5 @@ namespace fact
 
             void free() { mbedtls_entropy_free(&entropy); }
         };
-
-
-
-
-        class NetContext
-        {
-          mbedtls_net_context context;
-        public:
-
-          NetContext() { mbedtls_net_init(&context); }
-          //~NetContext() { mbedtls_net_free(&context); }
-
-          operator mbedtls_net_context&()
-          {
-              return context;
-          }
-
-          void free() {  mbedtls_net_free(&context); }
-
-          int bind(const char* bind_ip, const char* port, int proto)
-          {
-              return mbedtls_net_bind(&context, bind_ip, port, proto);
-          }
-
-          int accept(mbedtls_net_context& client_ctx, void* client_ip, size_t buf_size, size_t* ip_len)
-          {
-              return mbedtls_net_accept(&context, &client_ctx, client_ip, buf_size, ip_len);
-          }
-
-          int accept(mbedtls_net_context& client_ctx)
-          {
-              return mbedtls_net_accept(&context, &client_ctx, NULL, 0, NULL);
-          }
-        };
     }
 }
